aed52d1e17e1a16dc36220c01f2f0683f21b6458
CIBDO <bdokeita100@gmail.com>
2024-11-10 19:10:42 +0000

    log

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Rolemiddleware::class,
+            \App\Http\Middleware\RoleMiddleware::class,
         ],
 
         'api' => [
@@ -65,15 +65,15 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         /* 'role' => \App\Http\Middleware\CheckRole::class, */
-        'rolemiddleware' => \App\Http\Middleware\Rolemiddleware::class,
+        'rolemiddleware' => \App\Http\Middleware\RoleMiddleware::class,
 
     ];
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         /* 'role' => \App\Http\Middleware\CheckRole::class,  */
-        'rolemiddleware' => \App\Http\Middleware\Rolemiddleware::class,
-    
+        'rolemiddleware' => \App\Http\Middleware\RoleMiddleware::class,
+
         // autres middlewares
     ];
-   
+
 }
